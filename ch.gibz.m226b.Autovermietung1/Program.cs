8d4908068684e63f43817607f1b9e0ffc23dcefe
Janis Arnold <jarnold5@online.gibz.ch>
2020-08-17 11:45:17 +0200

    start

---
+++ ch.gibz.m226b.Autovermietung1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ch.gibz.m226b.Autovermietung1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
