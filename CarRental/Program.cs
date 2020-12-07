d18fc1bfb1a0e0706587966a39699c088d050298
Janis Arnold <jarnold5@online.gibz.ch>
2020-12-07 12:01:57 +0100

    1

--- CarRental/Program.cs
+++ CarRental/Program.cs
@@ -7,7 +7,7 @@ namespace CarRental
     {
         static void Main(string[] args)
         {
-            var _staffs = new List<Staff>()
+            List<Staff> _staffs = new List<Staff>()
             {
                 new Staff("Luca", "Daniel"),
                 new Staff("Laurin", "Bassler")
@@ -22,9 +22,41 @@ namespace CarRental
                     Console.WriteLine($"[{_staffs.IndexOf(item)}] {item.FirstName} {item.LastName}");
                 }
             }
-            Console.Write("Wählen Sie einen Mitarbeiter anhand von der Zuvorstehender Nummer: ");
-            string input = Console.ReadLine();
-            Console.WriteLine(input);
+            while (ChooseStaff(_staffs));
+
+            Console.WriteLine("Alle Fahrzeug Typen");
+            for (int i = 0; i < Enum.GetNames(typeof(VehicleTypes)).Length; i++)
+            {
+                Console.WriteLine($"[{i}] {(VehicleTypes)i}");
+            }
+            while (ChooseVehicleType());
+
+            bool ChooseStaff(List<Staff> _staffs)
+            {
+                try
+                {
+                    Console.Write("Wählen Sie einen Mitarbeiter anhand von der Zuvorstehender Nummer: ");
+                    string input = Console.ReadLine();
+                    _staffs[Convert.ToInt32(input)].IsFree = false;
+                    return false;
+                }
+                catch
+                {
+                    Console.WriteLine("Fehler!!!");
+                    return true;
+                }
+            }
+
+            bool ChooseVehicleType()
+            {
+                try
+                {
+                    Console.Write("Wählen Sie den Fahrzeugstyp anhand von der Zuvorstehender Nummer: ");
+                    string input = Console.ReadLine();
+                    return false;
+                }
+                catch { return true; }
+            }
         }
     }
 }
---
+++ CarRental/Vehicle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRental
+{
+    class Vehicle
+    {
+        private VehicleTypes VehicleType { get; }   
+        private string Brand { get; }
+        private string Model { get; }
+        private float Price { get; }
+
+        public Vehicle(VehicleTypes VehicleType, string Brand, string Model, float Price)
+        {
+            this.VehicleType = VehicleType;
+            this.Brand = Brand;
+            this.Model = Model;
+            this.Price = Price;
+        }
+    }
+    public enum VehicleTypes
+    {
+        Auto,
+        Motorrad,
+        Lastwagen,
+    }
+}
